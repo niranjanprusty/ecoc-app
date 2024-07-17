3f287ab33388b7a52afae915a52a9df417b4e0be
niranjanprusty <niranjan1.prusty@gmail.com>
2024-07-17 10:20:48 +0530

    base Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatSlideToggleModule,
+    MatIconModule, 
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecoc-app';
+  options: any;
+  constructor(private _formBuilder: FormBuilder) {
+  }
+  ngOnInit() {
+    this.options = this._formBuilder.group({
+      bottom: 0,
+      fixed: true,
+      top: 0,
+    });
+  }
+ 
+  
+  menuClick() {
+    alert('hi');
+  }
 }
